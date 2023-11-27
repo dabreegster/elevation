a000c002679daab08cc341c81c442b84cbf22b0b
Dustin Carlino <dabreegster@gmail.com>
2023-11-27 14:25:58 +0000

    Start a benchmark

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # elevation
+
 Explore options for reading elevation data in pure Rust
+
+- For A/B Street's super simple use, just calculate start and end elevation given a LineString input (in WGS84). The interface is just looking up a single elevation value at a time!
+	- Do bilinear interpolation at least
+- How can we validate the results?
+	- Generate an image, or run through a contour algorithm?
+- Can we parallelize?
+	- Decoding is mutable, so seemingly no
+	- How expensive is it memory-wise to load one of these in each thread?
+	- Just use the bbox bounds to generate a benchmark
+- Do we need to support more CRS or can we just transform stuff?
+- What should we do for contour cases? (Probably a different approach)
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::BufReader;
 
 use fs_err::File;
 use georaster::geotiff::{GeoTiffReader, RasterValue};
+use rand::Rng;
 
 fn main() {
     let seattle = "/home/dabreegster/abstreet/data/input/shared/elevation/kc_2016_lidar.tif";
@@ -9,26 +10,49 @@ fn main() {
 
     let mut reader = Reader::new(BufReader::new(File::open(uk).unwrap()));
 
-    // https://www.openstreetmap.org/node/719065360, Parliament hill, should be 93m
-    let lon = -0.1597557;
-    let lat = 51.5596469;
-    println!("{:?}", reader.get_height_for_lon_lat(lon, lat));
+    if false {
+        // https://www.openstreetmap.org/node/719065360, Parliament hill, should be 93m
+        let lon = -0.1597557;
+        let lat = 51.5596469;
+        println!("{:?}", reader.get_height_for_lon_lat(lon, lat));
+    }
+
+    if true {
+        benchmark(&mut reader, 500_000);
+    }
+}
+
+fn benchmark(reader: &mut Reader, iterations: usize) {
+    // TODO Might be flipped, the names are not right
+    let bottom_right = reader.get_bottom_right_lon_lat();
+    let x1 = reader.top_left[0].min(bottom_right[0]);
+    let x2 = reader.top_left[0].max(bottom_right[0]);
+    let y1 = reader.top_left[1].min(bottom_right[1]);
+    let y2 = reader.top_left[1].max(bottom_right[1]);
+
+    println!("Running {iterations} iterations");
+    let mut rng = rand::thread_rng();
+    for _ in 0..iterations {
+        let lon = rng.gen_range(x1..x2);
+        let lat = rng.gen_range(y1..y2);
+        reader.get_height_for_lon_lat(lon, lat);
+    }
 }
 
 struct Reader {
     tiff: GeoTiffReader<BufReader<File>>,
-    origin: [f64; 2],
+    top_left: [f64; 2],
     pixel_size: [f64; 2],
 }
 
 impl Reader {
     fn new(file: BufReader<File>) -> Self {
         let tiff = GeoTiffReader::open(file).unwrap();
-        let origin = tiff.origin().unwrap();
+        let top_left = tiff.origin().unwrap();
         let pixel_size = tiff.pixel_size().unwrap();
         Self {
             tiff,
-            origin,
+            top_left,
             pixel_size,
         }
     }
@@ -37,11 +61,18 @@ impl Reader {
     // Assumes the tiff is in EPSG:4326
     fn get_height_for_lon_lat(&mut self, lon: f64, lat: f64) -> Option<f32> {
         // Just linearly interpolate
-        let x = ((lon - self.origin[0]) / self.pixel_size[0]) as u32;
-        let y = ((lat - self.origin[1]) / self.pixel_size[1]) as u32;
+        let x = ((lon - self.top_left[0]) / self.pixel_size[0]) as u32;
+        let y = ((lat - self.top_left[1]) / self.pixel_size[1]) as u32;
         if let RasterValue::F32(height) = self.tiff.read_pixel(x, y) {
             return Some(height);
         }
         None
+    }
+
+    fn get_bottom_right_lon_lat(&self) -> [f64; 2] {
+        let (width, height) = self.tiff.images()[0].dimensions.unwrap();
+        let lon = self.top_left[0] + self.pixel_size[0] * (width as f64);
+        let lat = self.top_left[1] + self.pixel_size[1] * (height as f64);
+        [lon, lat]
     }
 }
